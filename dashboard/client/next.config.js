f6391d7c8f07d4dafc4405400cb2339e2d1bd928
Manuel Maset <manuelmaset2003@gmail.com>
2026-05-07 19:18:08 +0200

    Revert "upgrade dashboard next and clear audit"
    This reverts commit b5b32ddc29b0d3a5bb6764a66e6c60c4605ae53a.

--- dashboard/client/next.config.js
+++ dashboard/client/next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  outputFileTracingRoot: __dirname,
+  devIndicators: {
+    buildActivity: false,
+  },
 };
 
 module.exports = nextConfig;
